dfb9e37d3501a4203524f3e6d10f5e6479104d8c
Israel Sotomayor <sotoisra24@gmail.com>
2017-01-17 16:26:36 +0000

    chg: follow tf_aws_vpc nomeclature for default tag Name

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ variable "tags" {
 resource "aws_internet_gateway" "igw" {
   vpc_id = "${var.vpc_id}"
 
-  tags = "${merge(var.tags, map("Name", format("%s", var.name)))}"
+  tags = "${merge(var.tags, map("Name", format("%s-igw", var.name)))}"
 }
 
 output "igw_id" { value = "${aws_internet_gateway.igw.id}" }
